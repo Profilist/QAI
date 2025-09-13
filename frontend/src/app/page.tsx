656a5323828683e00e7c374d6263356d9f820710
JathanYao <jathansjyao@gmail.com>
2025-09-13 14:32:14 -0400

    add sidebar

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,5 +1,12 @@
+import Sidebar from "@/components/Sidebar";
+
 export default function Home() {
   return (
-    <div>Homepage</div>
+    <div className="flex min-h-screen">
+      <Sidebar />
+      <main className="flex-1 p-12">
+        Homepage
+      </main>
+    </div>
   );
 }
